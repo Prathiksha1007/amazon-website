c7392d82e711c3d0647944654d1f4e6f5c106445
Prat <prat300304@gmail.com>
2026-04-21 19:35:41 +0530

    Practiced oop in car.js file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,7 @@ import {renderOrderSummary} from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js'
 // import '../data/cart-class.js'; // runs all the code inside the folder
+import '../data/car.js';
 
 renderOrderSummary();
 renderPaymentSummary();
