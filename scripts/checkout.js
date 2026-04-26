a555cd374d846c381023116b7135092b4ee9282b
Prat <prat300304@gmail.com>
2026-04-26 13:14:27 +0530

    Load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,10 +2,13 @@ import {renderOrderSummary} from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js'
 // import '../data/cart-class.js'; // runs all the code inside the folder
-import '../data/car.js';
-import '../data/backend-practice.js';
+// import '../data/car.js';
+// import '../data/backend-practice.js';
+import { loadProducts } from '../data/products.js';
 
 
-renderOrderSummary();
-renderPaymentSummary();
-renderCheckoutHeader();
+loadProducts(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+    renderCheckoutHeader();
+});
