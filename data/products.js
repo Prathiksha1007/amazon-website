aaf3a6c07087f326993ce9f6299efe60d3908cc8
Prat <prat300304@gmail.com>
2026-04-27 17:14:26 +0530

    Convert loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -115,6 +115,31 @@ obj3.method();
 
 export let products = [];
 
+export function loadProductsFetch(){
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if(productDetails.type === "clothing"){
+        return new Clothing(productDetails);
+      } 
+      else if(productDetails.type === "appliances"){
+        return new Appliance(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+  });
+  return promise;
+}
+
+// ;loadProductsFetch().then(() => {
+//   console.log('next step');
+// })
+
 export function loadProducts(fun){
   const xhr = new XMLHttpRequest();
 
@@ -131,7 +156,7 @@ export function loadProducts(fun){
     });
 
     console.log('load products');
-    
+
     fun();
   });
 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,16 +4,12 @@ import {renderCheckoutHeader} from './checkout/checkoutHeader.js'
 // import '../data/cart-class.js'; // runs all the code inside the folder
 // import '../data/car.js';
 // import '../data/backend-practice.js';
-import { loadProducts } from '../data/products.js';
+import { loadProducts, loadProductsFetch } from '../data/products.js';
 import { loadCart } from '../data/cart.js';
 
 
 Promise.all([
-    new Promise((resolve) => {
-        loadProducts(() => {
-            resolve('value1');
-        });
-    }),
+    loadProductsFetch(),
     new Promise((resolve) => {
         loadCart(() => {
             resolve();
