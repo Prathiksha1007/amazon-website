0ab8b7720b54c60e583a89c0d226fff405729678
Prat <prat300304@gmail.com>
2026-04-27 14:52:02 +0530

    Learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,10 +5,53 @@ import {renderCheckoutHeader} from './checkout/checkoutHeader.js'
 // import '../data/car.js';
 // import '../data/backend-practice.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 
 
-loadProducts(() => {
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value1');
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+]).then((values) => {
+    console.log(values);
     renderOrderSummary();
     renderPaymentSummary();
     renderCheckoutHeader();
 });
+
+
+
+// new Promise((resolve) => {
+//     loadProducts(() => {
+//         resolve('value1');
+//     });
+
+// }).then((value) => {
+//     console.log(value);
+//     return new Promise((resolve) => {
+//         loadCart(() => {
+//             resolve();
+//         });
+//     });
+
+// }).then(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+//     renderCheckoutHeader();
+// });
+
+// loadProducts(() => {
+//     loadCart(() => {
+//         renderOrderSummary();
+//         renderPaymentSummary();
+//         renderCheckoutHeader();
+//     });
+// });
+
